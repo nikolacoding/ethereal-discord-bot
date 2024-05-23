6d0a55344c17583d5476ad1a0a654667d8593204
Nikola Markovi─ç <nikola.markovic@student.etf.unibl.org>
2024-05-23 02:05:20 +0200

    made unique namespaces for each driver file for general clarity; attempts to combat rate-limiting

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <dpp/dpp.h>
 #include <ctime>
+#include <windows.h>
 // Implementacija sa direktnim pristupom .cpp datotekama umjesto rada sa headerima zbog odredjenih problema pri kompilaciji
 #include "Helpers.cpp"
 #include "SlashCommands.cpp"
 #include "Constants.cpp"
 #include "token.cpp"
+
+/*
+	Standardni namespaceovi:
+	"hpf::" iz Helpers.cpp -> pomocne funkcije
+	"slc::" iz SlashCommands.cpp -> funkcije rada sa slash komandama
+	"con::" iz Constants.cpp -> konstante
+*/
 
 const extern std::string BOT_TOKEN; // from "token.cpp"
 // const std::string BOT_TOKEN = "-->YOUR TOKEN GOES HERE AS A STRING<--";
@@ -23,10 +31,10 @@ int main()
 				{ "info", "Ispisi detaljne informacije o sebi.", bot.me.id} //		/info
 			};
 
-			add_command_parameter(commands[0], "int", "opcija", "Opcije sazivanja: 0 -> SVI | 1 -> CS | 2 -> GTA | 3 -> PUBG", true);
-			add_command_parameter(commands[0], "string", "poruka", "Poruka pri sazivanju", false);
-			add_command_parameter(commands[1], "string", "ponudjeno", "Unesi rijeci odvojene zarezima za izbor", true);
-			add_command_parameter(commands[2], "string", "ponudjeno", "Unesi rijeci odvojene zarezima za rangiranje", true);
+			slc::add_command_parameter(commands[0], "int", "opcija", "Opcije sazivanja: 0 -> SVI | 1 -> CS | 2 -> GTA | 3 -> PUBG", true);
+			slc::add_command_parameter(commands[0], "string", "poruka", "Poruka pri sazivanju", false);
+			slc::add_command_parameter(commands[1], "string", "ponudjeno", "Unesi rijeci odvojene zarezima za izbor", true);
+			slc::add_command_parameter(commands[2], "string", "ponudjeno", "Unesi rijeci odvojene zarezima za rangiranje", true);
 
 			bot.global_bulk_command_create(commands);
 		}
@@ -34,29 +42,26 @@ int main()
 
 	// Slash komande
 	bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) -> dpp::task<void> {
-		if (get_command(event, "sazovi")) { // /sazovi <opcija> <poruka>
-			if (is_id_in_vector(event.command.get_issuing_user().id, admin_ids)) {
+		if (slc::get_command(event, "sazovi")) { // /sazovi <opcija> <poruka>
+			if (hpf::is_id_in_vector(event.command.get_issuing_user().id, con::ADMIN_IDS)) {
 				int64_t opcija;
 				std::string content;
 
-				if (!parameter_exists(event, "opcija")) {
+				if (!slc::parameter_exists(event, "opcija")) {
 					event.reply("Opcija nije unesena.");
 					co_return;
 				}
-				else opcija = get_parameter_value_int(event, "opcija");
-				bounds_handler_int(opcija, 4, 0, 4); // 0 - 4, default: 4
+				else opcija = slc::get_parameter_value_int(event, "opcija");
+				hpf::bounds_handler_int(opcija, 4, 0, 4); // 0 - 4, default: 4
 
-				// ako komanda nije dobila neobavezan parametar "poruka", 'content' uzima gorenavedenu podrazumijevanu vrijednost
-				if (!parameter_exists(event, "poruka")) content = CONTENT_DEFAULT_PREGAMENAME + GAMES[opcija] + CONTENT_DEFAULT_POSTGAMENAME;
-				else content = get_parameter_value_string(event, "poruka");
+				// ako komanda nije dobila neobavezan parametar "poruka", 'content' uzima podrazumijevanu vrijednost
+				if (!slc::parameter_exists(event, "poruka")) content = con::CONTENT_DEFAULT_PREGAMENAME + con::GAMES[opcija] + con::CONTENT_DEFAULT_POSTGAMENAME;
+				else content = slc::get_parameter_value_string(event, "poruka");
 
-				for (uint64_t id : ids[opcija]) { // prodji kroz vektor izabrane igre i posalji DM svakom clanu
-					dpp::snowflake user(id);
-					bot.direct_message_create(user, dpp::message(content), [event, user](const dpp::confirmation_callback_t& callback) {
-						if (callback.is_error()) {
-							event.reply("Nije moguce poslati DM jednom ili vise korisnika.");
-						}
-					});
+				for (uint64_t id : con::IDS[opcija]) { // prodji kroz vektor izabrane igre i posalji DM svakom clanu
+					dpp::snowflake user_sfid(id);
+					bot.direct_message_create(user_sfid, dpp::message(content));
+					Sleep(1000); // ne znam da li ovo pomaze da se obidje potencijalni rate limit, pogledati poslije, trenutno radi normalno
 				}
 
 				event.reply("Sazvani!");
@@ -68,14 +73,14 @@ int main()
 			}
 		}
 		
-		if (get_command(event, "pickone")) { // /pickone <rijeci odvojene razmakom>
-			if (!parameter_exists(event, "ponudjeno")) {
+		if (slc::get_command(event, "pickone")) { // /pickone <rijeci odvojene razmakom>
+			if (!slc::parameter_exists(event, "ponudjeno")) {
 				event.reply("Nisi zadao izbor.");
 				co_return;
 			}
 			else {
-				const std::string param = get_parameter_value_string(event, "ponudjeno");
-				const std::vector<std::string> words = split_string(param);
+				const std::string param = slc::get_parameter_value_string(event, "ponudjeno");
+				const std::vector<std::string> words = hpf::split_string(param);
 
 				int index = std::rand() % words.size();
 				std::string choice = words[index];
@@ -85,14 +90,14 @@ int main()
 			}
 		}
 
-		if (get_command(event, "rankthem")) { // /rankthem <rijeci odvojene razmakom>
-			if (!parameter_exists(event, "ponudjeno")) {
+		if (slc::get_command(event, "rankthem")) { // /rankthem <rijeci odvojene razmakom>
+			if (!slc::parameter_exists(event, "ponudjeno")) {
 				event.reply("Nisi zadao izbor.");
 				co_return;
 			}
 			else {
-				const std::string param = get_parameter_value_string(event, "ponudjeno");
-				std::vector<std::string> words = split_string(param);
+				const std::string param = slc::get_parameter_value_string(event, "ponudjeno");
+				std::vector<std::string> words = hpf::split_string(param);
 				std::string reply = "";
 				int rank = 1;
 
@@ -108,15 +113,15 @@ int main()
 			}
 		}
 		
-		if (get_command(event, "info")) {
+		if (slc::get_command(event, "info")) {
 			const dpp::user issuing_user = event.command.get_issuing_user();
 			const dpp::resolved_user r_issuing_user(issuing_user);
 			int64_t server_join_date_raw = static_cast<int64_t>(r_issuing_user.member.joined_at);
 			// ^ ovo je 0 iz nekog razloga
 
 			const std::string username = issuing_user.global_name;
-			const std::string account_creation_date = format_unix_time(issuing_user.get_creation_time(), true); // vrijeme pravljenja naloga
-			const std::string server_join_date = format_unix_time(server_join_date_raw);
+			const std::string account_creation_date = hpf::format_unix_time(issuing_user.get_creation_time(), true); // vrijeme pravljenja naloga
+			const std::string server_join_date = hpf::format_unix_time(server_join_date_raw);
 			const std::string user_profile_picture_url = issuing_user.get_avatar_url();
 
 			dpp::embed embed = dpp::embed().
@@ -134,7 +139,7 @@ int main()
 					server_join_date,
 					true
 				).
-				set_footer(dpp::embed_footer().set_text("Clan je " + std::to_string(days_since(server_join_date_raw)) + " dana."));
+				set_footer(dpp::embed_footer().set_text("Clan je " + std::to_string(hpf::days_since(server_join_date_raw)) + " dana."));
 			embed.timestamp = time(0);
 			bot.message_create(dpp::message(event.command.channel_id, embed));
 
@@ -151,21 +156,21 @@ int main()
 		const dpp::message msg = event.msg;
 		dpp::channel *channelptr = dpp::find_channel(msg.channel_id); // pokazivac na instancu trenutnog kanala
 
-		const uint64_t self_id = BOT_ID;
+		const uint64_t self_id = con::BOT_ID;
 		const uint64_t msg_author_id = msg.author.id;
 
 		const std::string msg_str_raw = msg.content; // raw message content
-		const std::string msg_str_parsed = lowercase_parse(msg.content); // all-lowercase message content
+		const std::string msg_str_parsed = hpf::lowercase_parse(msg.content); // all-lowercase message content
 
-		log_message(msg.author.username, msg_str_raw, channelptr);
-		if (contains_substring(msg_str_parsed, SAY_DO_PROMPT) && msg_author_id != self_id) {
-			std::string reply = say_do_string_parser(msg_str_raw, SAY_DO_PROMPT);
+		hpf::log_message(msg.author.username, msg_str_raw, channelptr);
+		if (hpf::contains_substring(msg_str_parsed, con::SAY_DO_PROMPT) && msg_author_id != self_id) {
+			std::string reply = hpf::say_do_string_parser(msg_str_raw, con::SAY_DO_PROMPT);
 			event.reply(reply, false);
 			return;
 		}
 
-		if (msg_author_id == BUREK_ID && contains_substring(msg_str_parsed, BUREK_PROMPT)) {
-			event.reply("gej", false);
+		if (msg_author_id == con::BUREK_ID && hpf::contains_substring(msg_str_parsed, con::BUREK_PROMPT)) {
+			event.reply(con::BUREK_REPLY, false);
 			return;
 		}
 	});
--- MyBot/SlashCommands.cpp
+++ MyBot/SlashCommands.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -6,26 +7,36 @@
 
 #include "Constants.cpp"
 
-// Provjera poziva komande prema imenu
-extern bool get_command(const dpp::slashcommand_t& event, const std::string command_name) {
-	if (event.command.get_command_name() == command_name) return true;
-	return false;
-}
+namespace slc {
+	// Provjera poziva komande prema imenu
+	extern bool get_command(const dpp::slashcommand_t& event, const std::string command_name) {
+		if (event.command.get_command_name() == command_name) return true;
+		return false;
+	}
 
-// Provjera postojanja parametra prema imenu
-extern bool parameter_exists(const dpp::slashcommand_t& event, const std::string parameter_name) {
-	if (event.get_parameter(parameter_name).index()) return true;
-	return false;
-}
+	// Provjera postojanja parametra prema imenu
+	extern bool parameter_exists(const dpp::slashcommand_t& event, const std::string parameter_name) {
+		if (event.get_parameter(parameter_name).index()) return true;
+		return false;
+	}
 
-// Uzimanje potpune vrijednosti korisnicki-definisanog parametra tipa uint64_t
-extern int64_t get_parameter_value_int(const dpp::slashcommand_t& event, const std::string parameter_name) {
-	int64_t return_value = std::get<int64_t>(event.get_parameter(parameter_name));
-	return return_value;
-}
+	// Uzimanje potpune vrijednosti korisnicki-definisanog parametra tipa uint64_t
+	extern int64_t get_parameter_value_int(const dpp::slashcommand_t& event, const std::string parameter_name) {
+		int64_t return_value = std::get<int64_t>(event.get_parameter(parameter_name));
+		return return_value;
+	}
 
-// Uzimanje potpune vrijednosti korisnicki-definisanog parametra tipa std::string
-extern std::string get_parameter_value_string(const dpp::slashcommand_t& event, const std::string parameter_name) {
-	std::string return_value = std::get<std::string>(event.get_parameter(parameter_name));
-	return return_value;
+	// Uzimanje potpune vrijednosti korisnicki-definisanog parametra tipa std::string
+	extern std::string get_parameter_value_string(const dpp::slashcommand_t& event, const std::string parameter_name) {
+		std::string return_value = std::get<std::string>(event.get_parameter(parameter_name));
+		return return_value;
+	}
+
+	extern void add_command_parameter(dpp::slashcommand& command, const std::string type, const std::string command_name, const std::string parameter_description, const bool required) {
+		if (hpf::lowercase_parse(type) == "int")
+			command.add_option(dpp::command_option(dpp::co_integer, command_name, parameter_description, required));
+
+		if (hpf::lowercase_parse(type) == "string")
+			command.add_option(dpp::command_option(dpp::co_string, command_name, parameter_description, required));
+	}
 }
